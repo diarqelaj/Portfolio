f61443003ed3ad2ac4aee1a1d8d3b3fed5127401
Dijar <diarqelaj15@gmail.com>
2025-04-22 18:24:50 +0200

    Analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <ClientBody>{children}</ClientBody>
+      <Analytics />
     </html>
   );
 }
